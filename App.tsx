a3404cbccbb136f95a096e81dad42cec7e0be5ba
Yudi Alviska <yudi.a@weefer.co.id>
2023-05-09 10:16:42 +0700

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Realm from 'realm';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
