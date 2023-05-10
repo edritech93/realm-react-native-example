02d589b649abd336fd8f577bc6bfc5eacfc943f6
Yudi Alviska <yudi.a@weefer.co.id>
2023-05-10 12:53:36 +0700

    add test write/read data

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,10 +1,5 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
-import {create} from 'react-test-renderer';
+import {render, waitFor} from '@testing-library/react-native';
 import App from '../src/screens/App';
 import Profile from '../src/database/Profile.database';
 import Realm from 'realm';
@@ -12,7 +7,7 @@ import Realm from 'realm';
 const config = {
   schema: [Profile],
 };
-let realm: any;
+let realm: Realm;
 
 describe('Test App', () => {
   beforeEach(async () => {
@@ -28,13 +23,28 @@ describe('Test App', () => {
     }
   });
 
+  test('write/read', async () => {
+    realm.write(() => {
+      realm.create('Profile', {
+        name: 'Yudi',
+        _id: new Realm.BSON.ObjectId(),
+      });
+    });
+    await waitFor(() => {
+      const data = realm.objects('Profile').toJSON();
+      expect(data).toHaveLength(1);
+      expect(data[0].name).toEqual('Yudi');
+    });
+  });
+
   test('Close a Realm', async () => {
+    console.log('realm.isClosed => ', realm);
     expect(realm.isClosed).toBe(false);
     realm.close();
     expect(realm.isClosed).toBe(true);
   });
 
   test('renders correctly', () => {
-    create(<App />);
+    render(<App />);
   });
 });
